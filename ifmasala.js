0a343648853d50efdf95b537618879e03fca44d2
Maxvin10 <maxvin404@gmail.com>
2025-10-31 02:38:47 +0500

    if task

--- ifmasala.js
+++ ifmasala.js
@@ -108,3 +108,235 @@
 // console.log(b);
 
 // 10-masala
+// let a = 121, b = 322;
+// if(a === b){
+//     console.log(0);
+    
+// } else{
+//     console.log(a = a + b);
+//     console.log(b = a + b);
+    
+// }
+
+// 11-masala
+// let a = 3131, b = 422;
+// if(a !== b){
+//     console.log(a = a);
+//     console.log(b = a);
+// }
+// if(a === b){
+//     console.log(0);
+// }
+
+
+// 12-masala
+// let a = 121, b = 31121, c = 21;
+// console.log(Math.min({a,b,c}));
+
+// 13-masala
+// let a = 1212, b = 313, c = 3131;
+// if(a > b < c){
+//     console.log(b);
+    
+// }
+// if(b > a < c){
+//     console.log(a);
+// }
+// if(a > c < b){
+//     console.log(c);
+// }
+
+// 14-masala
+// let a = 121, b = 423, c = 3231;
+// console.log(Math.max({a,b,c}));
+// console.log(Math.min({a,b,c}));
+
+// 15-masala
+// let a = 432, b = 3321, c = 3111;
+// if(a > b > c || b > a > c){
+//     console.log(a + b);
+    
+// }
+// if(c > a > b || a > b > c){
+//     console.log(a+c);
+// }
+// if(b > a > c || c > b > a){
+//     console.log(b + c);
+// }
+
+// 16-masala
+// let a = 321, b = 42, c = 32;
+// if(a > b > c){
+//     a*=2;
+//     b*=2;
+//     c*=2;
+// console.log(a,b,c);
+
+// } else{
+//     a = -a;
+//     b = -b;
+//     c = -c;
+// }
+
+// 17-masala
+// let a = 12332, b = 32311, c = 42232;
+// if(a > b > c){
+//     console.log(a*2,b*2,c*2);
+    
+// } else{
+//     console.log(-Math.abs(a));
+//     console.log(-Math.abs(b));
+//     console.log(-Math.abs(c));
+// }
+
+// 18-masala
+// let a = 3, b = 4, c = 3;
+// if(a === b){
+//     console.log(3);
+    
+// }  
+// if(b === c){
+//     console.log(1);
+    
+// } 
+// if(a === c){
+//     console.log(2);
+    
+// }
+
+// 19-masala
+// let a = 1, b = 2, c = 1, d = 5;
+// if(a === b || b === c || a === c){
+//     console.log(4);
+// }
+// if(b === c || c === d || b === d){
+//     console.log(1);
+// }
+// if(c === d || d === a){
+//     console.log(2);
+// }
+// if(d === a || a === b){
+//     console.log(3);
+// }
+
+
+// 20-masala
+// let a = 5, b = 3, c = 2;
+// console.log(Math.abs(b-a));
+// console.log(Math.abs(c-a));
+
+// 21-masala
+// let x = 2, y = 4;
+// if(x === 0 && y === 0){
+//     console.log(0);
+// }
+// if(x > 0 || x < 0 || y > 0 || y < 0){
+//     console.log(1);
+// } else {
+//     console.log(3);
+// }
+
+// 22 -masala
+// let x = 4, y = 5;
+// if(x < 0 && y > 0){
+//     console.log("1-chorak");
+// }
+// if(x > 0 && y > 0){
+//     console.log("2-chorak");
+// }
+// if(x < 0 && y < 0){
+//     console.log("3-chorak");
+// }
+// if(x > 0 && y < 0){
+//     console.log("4-chorak")
+// }
+
+
+// 23-masala
+// let a = 50, b = 60, c = 32;
+// let x = 180 - (a + b + c);
+// console.log(`To'rtinchi burchak: ${x}`);
+
+
+// 24-masala
+// let x = 5;
+// if(x > 0){
+//     console.log(2 * Math.sin(x))
+// }
+// if(x <= 0){
+//     console.log(6 - x);
+// }
+
+
+// 25-masala
+// let x = 43;
+// if(x < -2 || x > 2){
+//     console.log(2 * x);
+// } else {
+//     console.log(-3 * x);
+// }
+
+// 26-masala
+// let x = 3;
+// if(x <= 0){
+//     console.log(-x);
+// }
+// if(0 < x < 2){
+//     console.log(x**2);
+// }
+// if(x >= 2){
+//     console.log(4);
+// }
+
+
+// 27-masala
+// let x = 9;
+// if(x < 0){
+//     console.log(0);
+// }
+// if()
+
+// 28-masala
+// let x = 400;
+// if(x % 400 == 0 && x % 4 == 0 && x % 100 != 0){
+//     console.log(366);
+// } else{
+//     console.log(365);
+// }
+
+// 29-masala
+// let n = 44;
+// if(n > 0 && n % 2 == 0){
+//     console.log("Musbat juft");
+// }
+// if(n < 0 && n % 2 != 0){
+//     console.log("Manfiy toq");
+// }
+// if(n === 0){
+//     console.log("Nol")
+// } else{
+//     console.log("):")
+// }
+
+
+// 30-masala
+// let n = 123;
+// if(n < 10 && n % 2 != 0 && n != 0){
+//     console.log("Bir xonali toq");
+// }
+// if(n < 10 && n % 2 == 0 && n != 0){
+//     console.log("Bir xonali juft");
+// }
+// if(9 < n && n < 100 && n % 2 != 0 && n != 0){
+//     console.log("Ikki xonali toq");
+// }
+// if(9 < n && n < 100 && n % 2 === 0 && n != 0){
+//     console.log("Ikki xonali juft");
+// }
+// if(100 < n && n < 1000 && n % 2 !== 0 && n != 0){
+//     console.log("Uch xonali toq");
+// }
+// if(100 < n && n < 1000 && n % 2 === 0 && n != 0){
+//     console.log("Uch xonali juft");
+// }
+
