616c3d002c820ce6e81d0685033910fad9bf6044
Maxvin10 <maxvin404@gmail.com>
2025-11-05 17:08:02 +0500

    function tasks

--- functiontasks.js
+++ functiontasks.js
@@ -43,5 +43,104 @@
 // console.log(Rectps(3,4,5,6));
 
 // 6-masala
+// function DigitCountSum(K) {
+//   let C = 0;
+//   let S = 0;
+
+//   while (K > 0) {
+//     let digit = K % 10; 
+//     S += digit;         
+//     C++;                
+//     K = Math.floor(K / 10); 
+//   }
+
+//   return { C, S };
+// }
+// console.log(DigitCountSum(5));
+
+
+// 7-masala
+// function InvDigits(k){
+//     let a = k.reverse();
+//     return a;
+// }
+// console.log(InvDigits(53));
+
+
+// 8-masala
+// function addright(n, d){
+//     let f = n.toString();
+//     let g = d.toString();
+//     return f + g;
+// }
+// console.log(addright(123,4));
+
+
+// 9-masala
+// function addright(n, d){
+//     let f = n.toString();
+//     let g = d.toString();
+//     return g + f;
+// }
+// console.log(addright(123,4));
+
+// 10-masala
+// function swap(a, b) {
+//   let temp = a;
+//   a = b;
+//   b = temp;
+//   return [a, b]; 
+// }
+// console.log(2,3);
+
+
+// 11-masala
+// function Minmax(x,y){
+//     let d = Math.max(x,y);
+//     let f = Math.min(x,y);
+//     return d,f;
+// }
+// console.log(43,56);
+
+
+// 12-masala
+// function Sortinc(a,b,c){
+//     if(a <= b <= c){
+//         return a,b,c;
+//     }
+// }
+// console.log(2,3,4);
+
+
+// 13-masala
+// function Sortinc(a,b,c){
+//     if(a >= b >= c){
+//         return a,b,c;
+//     }
+// }
+// console.log(2,3,4);
+
+// 14-masala
+// function ShiftRight(a,b,c){
+//     a = b;
+//     b = c;
+//     a = c;
+//     return a,b,c;
+// }
+// console.log(1,2,3);
+
+// 15-masala
+// function ShiftRight(a,b,c){
+//     a = c;
+//     c = b;
+//     b = a;
+//     return a,b,c;
+// }
+// console.log(1,2,3);
+
+
+
+
+
 
 
